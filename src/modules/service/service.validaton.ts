d88da24ea78c1d6f807d89f2f507052e8787cb94
biplobrana007 <mubiplobrana@gmail.com>
2026-07-11 00:22:46 +0600

    input validation in service module is done

---
+++ src/modules/service/service.validaton.ts
@@ -0,0 +1,71 @@
+import { Request } from "express";
+import {
+  validatePositiveNumber,
+  validateRequiredFields,
+  validateStringLength,
+  validateTypes,
+  validateUUID,
+} from "../../utils/validation.utils";
+
+const createServiceValidation = (req: Request) => {
+  const { title, description, price, duration, categoryId } = req.body;
+
+  validateRequiredFields(req.body, [
+    "title",
+    "description",
+    "price",
+    "duration",
+    "categoryId",
+  ]);
+
+  validateTypes(req.body, {
+    title: "string",
+    description: "string",
+    price: "string",
+    duration: "number",
+    categoryId: "string",
+  });
+
+  validatePositiveNumber("Price", Number(price));
+  validatePositiveNumber("Duration", duration);
+  validateStringLength("Description", description, 20, 150);
+  validateStringLength("Title", title, 5, 50);
+
+  validateUUID("CategoryId", categoryId);
+};
+const getServiceByIdValidation = (req: Request) => {
+  const serviceId = req.params.id;
+  validateUUID("ServiceId", serviceId as string);
+};
+const updateServiceValidation = (req: Request) => {
+//   const serviceId = req.params.id;
+//   const { title, description, price, duration, categoryId } = req.body;
+
+//   validateUUID("ServiceId", serviceId as string);
+
+//   validateTypes(req.body, {
+//     title: "string",
+//     description: "string",
+//     price: "string",
+//     duration: "number",
+//     categoryId: "string",
+//   });
+
+//   validatePositiveNumber("Price", Number(price));
+//   validatePositiveNumber("Duration", duration);
+//   validateStringLength("Description", description, 20, 150);
+//   validateStringLength("Title", title, 10, 50);
+
+//   validateUUID("CategoryId", categoryId);
+};
+const deleteServiceValidation = (req: Request) => {
+  const serviceId = req.params.id;
+  validateUUID("ServiceId", serviceId as string);
+};
+
+export const serviceValidations = {
+  createServiceValidation,
+  getServiceByIdValidation,
+  updateServiceValidation,
+  deleteServiceValidation,
+};
--- src/modules/service/servoce.route.ts
+++ src/modules/service/servoce.route.ts
@@ -2,16 +2,34 @@ import { Router } from "express";
 import auth from "../../middleware/auth";
 import { Role } from "../../../generated/prisma/enums";
 import { serviceControllers } from "./service.controller";
+import validateInput from "../../middleware/validiateInput";
+import { serviceValidations } from "./service.validaton";
 
 const router = Router();
 
-router.post("/", auth(Role.TECHNICIAN), serviceControllers.createService);
+router.post(
+  "/",
+  auth(Role.TECHNICIAN),
+  validateInput(serviceValidations.createServiceValidation),
+  serviceControllers.createService
+);
 router.get("/", serviceControllers.getAllService);
-router.get("/:id", serviceControllers.getServiceById);
-router.patch("/:id", auth(Role.TECHNICIAN), serviceControllers.updateService);
+
+router.get(
+  "/:id",
+  validateInput(serviceValidations.getServiceByIdValidation),
+  serviceControllers.getServiceById
+);
+router.patch(
+  "/:id",
+  auth(Role.TECHNICIAN),
+  validateInput(serviceValidations.updateServiceValidation),
+  serviceControllers.updateService
+);
 router.delete(
   "/:id",
   auth(Role.TECHNICIAN, Role.ADMIN),
+  validateInput(serviceValidations.deleteServiceValidation),
   serviceControllers.deleteService
 );
 
